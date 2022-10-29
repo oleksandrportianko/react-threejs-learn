3f3d69dcacf0995c1276a1b5d10a30e6f9034b28
Oleksandr <portianko123@gmail.com>
2022-10-29 13:13:18 +0300

    added interaction to yellow plaster

--- src/components/canvas/canvas.component.js
+++ src/components/canvas/canvas.component.js
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Canvas } from '@react-three/fiber'
 
 import YellowPlaster from '../yellow-plaster/yellow-plaster.component'
 
 const CanvasComponent = () => {
-    const [rotatePlaster, setRoratePlaster] = useState(2)
-
-    const changeRotatePlaster = () => {
-        setRoratePlaster(rotatePlaster + 1)
-    }
-
     return (
         <Canvas style={{backgroundColor: 'white', minHeight: '100vh'}}>
             <pointLight position={[12, 10, 10]} />
-            <mesh onClick={changeRotatePlaster} position={[-1, 0, 2]} rotation={[Math.PI / 2, 0, 0]}>
-                <YellowPlaster />
-            </mesh>
+            <YellowPlaster />
         </Canvas>
     )
 }
--- src/components/yellow-plaster/yellow-plaster.component.js
+++ src/components/yellow-plaster/yellow-plaster.component.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useFrame, useThree } from '@react-three/fiber'
 
-export default function YellowPlaster(props) {
-  const { scene, nodes, materials } = useGLTF('/models/yellow-plaster-3d-model/yellow_plaster_4k.gltf')
+export default function YellowPlaster() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 480, y: 190 })
+  const [trigger, setTrigger] = useState(false)
+  const { scene } = useGLTF('/models/yellow-plaster-3d-model/yellow_plaster_4k.gltf')
+  const yellowPlasterRef = useRef()
 
-  console.log(nodes)
-  console.log(scene)
-  console.log(materials)
+  console.log(cursorPosition)
 
+  const handleMovePointer = (e) => {
+    console.log('pointer move', e)
+    setCursorPosition({ x: e.screenX, y: e.screenY })
+    setTrigger(true)
+    setTimeout(() => {
+      setTrigger(false)
+    }, 1000)
+  }
+
+  useFrame(() => {
+    if(yellowPlasterRef.current && trigger) {
+      // console.log('yellowPlasterRef.current', yellowPlasterRef.current)
+      if (cursorPosition.x > 480) {
+        yellowPlasterRef.current.rotation.x += 0.01;
+      } else {
+        yellowPlasterRef.current.rotation.x -= 0.01;
+      }
+      if (cursorPosition.y > 190) {
+        yellowPlasterRef.current.rotation.y += 0.01;
+      } else {
+        yellowPlasterRef.current.rotation.y -= 0.01;
+      }
+    }
+  });
 
   return (
-    <primitive object={scene} />
+    <mesh onPointerMove={handleMovePointer} ref={yellowPlasterRef} position={[-1, 0, 2]} rotation={[Math.PI / 2, 0, 0]}>
+      <primitive object={scene} />
+    </mesh>
   ) 
 }
